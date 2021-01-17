ac1cd96333b32c5b4426b3a0e9783f50fe5454a9
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-01-17 14:59:24 +0530

    counter

---
+++ javascript _projects/project_2/app.js
@@ -0,0 +1,45 @@
+let count =0;
+
+const value=document.querySelector("#value");
+const btns=document.querySelectorAll(".btn");
+
+
+//console.log(btns);
+
+
+btns.forEach(function(btn){
+     
+     btn.addEventListener('click',function(e){
+         const styles= e.currentTarget.classList;
+         if(styles.contains('decrease'))
+         {
+               count=count-1;
+         }
+         else
+         if(styles.contains('increase'))
+         {
+               count=count+1;
+         }  
+         else   
+         {
+             count=0;
+         }
+
+         if(count>0)
+         {
+              value.style.color='green';
+         }
+         else
+         if(count<0)
+         {
+               value.style.color='red';
+         }
+         else
+         {
+              value.style.color='black';
+         }
+         
+         value.textContent=count;
+
+     });
+});
